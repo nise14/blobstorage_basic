7750cef684916c42a4ef9b009bb6864da185ce9f
Nicolas <nise14@htomail.com>
2023-10-02 10:35:48 -0500

    first commit

---
+++ democoreapp/Program.cs
@@ -0,0 +1,11 @@
+﻿using Azure.Storage.Blobs;
+
+string connection = "DefaultEndpointsProtocol=https;AccountName=demoappcore;AccountKey=NRxUzd2doH+FDsQYiVNK+25vBugCgdL3LriRNkS+jpAqLXAMVOXVh4DG7aVn0O7hQ+TBo7kFKh7W+ASt8CySmg==;EndpointSuffix=core.windows.net";
+Console.WriteLine("Ingrese el nombre del BLOB: ");
+string name = Console.ReadLine();
+Console.WriteLine(name);
+BlobServiceClient blobServiceClient = new BlobServiceClient(connection);
+
+await blobServiceClient.CreateBlobContainerAsync(name);
+
+Console.WriteLine($"Se creo el blob con el nombre de {name}");
